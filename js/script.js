5e95f9c3be5689c4c5c400ac5adb4b26dfc1e9d3
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2019-03-15 09:57:10 +0100

    ny popup

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@
 $(document).ready(function() {
   $('.basic-multiple').select2();
 });
-
+// Popup-nudge slide up when page loads
+function slideUp(){
+  document.querySelector('.popup-nudge').style.bottom = "5%";
+};
+document.querySelector('.create').addEventListener('click', function(){
+newDoc2();
+});
 document.querySelector('#sign-up').oninput = function(){
   let req1 = document.querySelector('#first-name').value;
   let req2 = document.querySelector('#last-name').value;
@@ -111,7 +117,9 @@ function createAccount() {
 function newDoc() {
   window.location.assign("https://xd.adobe.com/view/2a7a43c6-984d-45cf-4a83-18343a645b8d-5c4b/?fullscreen")
 }
-
+function newDoc2() {
+  window.location.assign("https://jais-e.dk/grundfos/login.html")
+}
 
 /*function validateEmail(email) {
   var expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
