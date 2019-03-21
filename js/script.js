b43685aec86fffc05af64f8b42c78e9745f1c726
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2019-03-21 23:00:08 +0100

    cc

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 // jQuery - assign Select2 library to a class
 $(document).ready(function() {
   $('.basic-multiple').select2();
+
 });
 
-// Popup-nudge slide up when page loads - !! DEACTIVATED !!
-/*function slideUp(){
-  document.querySelector('.popup-nudge').style.bottom = "5%";
-};*/
+// Popup-nudge slide up when page loads
+function slideUp(){
+  document.querySelector('.popup-nudge').style.bottom = "-0.5%";
+};
 
 document.querySelector('#sign-up').oninput = function(){
   let req1 = document.querySelector('#first-name').value;
@@ -102,7 +103,7 @@ function createAccount() {
   };
   users.push(newAccount);
   console.log(users);
-  newDoc();
+  newDoc2();
 }
   // If checbox is unchecked
   else{
@@ -111,12 +112,14 @@ function createAccount() {
 
 };
 function newDoc() {
-  window.location.assign("https://xd.adobe.com/view/739ed267-4b26-492b-6f8b-2b8d8afad5b9-05af/?fbclid=IwAR2dprFDiIEHGOF1lmxCrH9AprOgYvCG049UoJagFR8ddMFakjSpce43VUI&fullscreen");
-}
+  window.location.assign("login.html");
+};
 function newDoc2() {
-  window.location.assign("http://jais-e.dk/grundfos/login.html");
-}
-
+  window.location.assign("page2.html");
+};
+function newXd() {
+  window.location.assign("https://xd.adobe.com/view/739ed267-4b26-492b-6f8b-2b8d8afad5b9-05af/?fbclid=IwAR399CyPfnhKcphcXH7S8xHOjPwZCt5vVPl_hCLiKfb-d8fT10BlJaBTwfY&fullscreen");
+};
 /*function validateEmail(email) {
   var expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
   return expression.test(String(email).toLowerCase())
